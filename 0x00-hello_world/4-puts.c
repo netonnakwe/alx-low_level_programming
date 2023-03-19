75bf829d92c2cd59e52383212f3788a9f6c2d89b
Netochukwu Nnakwe <netonnakwe@gmail.com>
2023-03-19 04:57:59 -0700

    4-puts.c added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#!/bin/bash
+#include <stdio.h>
+/*
+ * Print "Programming is like building a mulrilingual puzzle
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+}
