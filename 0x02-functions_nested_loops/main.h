bf84c90cfa299c734efa4d58f15c829051543ef7
Netochukwu Nnakwe <netonnakwe@gmail.com>
2023-03-21 23:33:45 -0700

    Updated main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,7 +8,7 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
-int _print_sign(int n);
+int print_sign(int n);
 int _abs(int);
 int _print_last_digit(int);
 void print_to_98(int n);
