b62517ca3b13777fb6760dd5a57eba7d71bf8d73
Netochukwu Nnakwe <netonnakwe@gmail.com>
2023-03-21 22:57:17 -0700

    2-print_alphabet_x10 added

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+	int n, co;
+
+	co = 0;
+
+	while (co < 10)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		co++;
+		_putchar('\n');
+	}
+}
