1a201895d9a4e9696721ee3979f432fbf4310f6e
Netochukwu Nnakwe <netonnakwe@gmail.com>
2023-03-22 16:30:03 -0700

    Betty documentation added

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * print_last_digit - function that prints the last digit of a number
+ * @ernest: number's last digit result
+ * Return: value of the last digit
+ */
 int print_last_digit(int ernest)
 {
 	int eks;
