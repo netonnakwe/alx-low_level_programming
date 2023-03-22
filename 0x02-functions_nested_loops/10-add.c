1877458887a5b8efd42c0d3238a8ae294014f2f1
Netochukwu Nnakwe <netonnakwe@gmail.com>
2023-03-22 16:11:06 -0700

    Description for parameters added

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * add - a function that adds 2 integers one and two and returns the result
- * one: first integer input
- * two: second integer input
+ * @one: first integer input
+ * @two: second integer input
  * Return: one + two
  */
 int add(int one, int two)
