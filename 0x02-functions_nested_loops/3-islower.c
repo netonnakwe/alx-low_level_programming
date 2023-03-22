76c68dad098b93113078be7e3254dcbbd4354c0e
Netochukwu Nnakwe <netonnakwe@gmail.com>
2023-03-22 16:49:15 -0700

    Betty documentation added

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * _islower - a function that checks for lowercase
+ * @c: input
+ * Return: 1 if lowercase, 0 if otherwise
+ */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
