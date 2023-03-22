ee723c322f263902f318137eb2eaf5f3180037cb
Netochukwu Nnakwe <netonnakwe@gmail.com>
2023-03-22 16:59:26 -0700

    Betty documentation added

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,8 @@
 #include "main.h"
-
+/**
+ * print_alphabet_x10 - a function that prints the alphabet 10 times
+ * Return: From a to z is printed 10 times
+ */
 void print_alphabet_x10(void)
 {
 	int n, co;
