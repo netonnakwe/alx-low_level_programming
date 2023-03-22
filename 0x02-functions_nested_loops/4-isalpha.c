c3af8f787a4e3735162811bbccd13b229bd13447
Netochukwu Nnakwe <netonnakwe@gmail.com>
2023-03-22 16:44:08 -0700

    Betty documentation added

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * _isalpha - a function that checks for alphabetic characters
+ * @e: input
+ * Return: 1 if e is a letter (both lowercase or uppercase), 0 if otherwise
+ */
 int _isalpha(int e)
 {
 	if ((e >= 'a' && e <= 'z') || (e >= 'A' && e <= 'Z'))
